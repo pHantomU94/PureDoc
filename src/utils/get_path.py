9f38fcab710f02ec84a7a468309cb64cd63e98cd
hushegnjie <dzzxhushegnjie@126.com>
2026-02-10 22:43:54 +0800

    Fix: fix pandoc path path

--- src/utils/get_path.py
+++ src/utils/get_path.py
@@ -1,8 +1,8 @@
 import os
-import sys
+import stat
 from pathlib import Path
 
-def get_resource_path(relative_path):
+def get_resource_path(relative_path) -> str:
     """
     get assets real url
     """
@@ -14,8 +14,19 @@ def get_resource_path(relative_path):
     
     return str(resource_path)
 
+def get_pandoc_path() -> str:
+    # 定位到 assets/bin/pandoc
+    path = get_resource_path("bin/pandoc")
+    
+    # 关键一步：确保打包后的二进制文件具有“可执行”权限
+    if os.path.exists(path):
+        current_stat = os.stat(path)
+        os.chmod(path, current_stat.st_mode | stat.S_IEXEC)
+        
+    return path
+
 # get download path
-def get_download_path():
+def get_download_path() -> str:
     """
     get current user's downloads path
     """
